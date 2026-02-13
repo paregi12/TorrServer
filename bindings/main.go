4e86eec3e86c289314f8979da39b33a44fc9e5f0
paregi12 <paregi1252@gmail.com>
2026-02-13 11:19:54 +0530

    Create main.go

---
+++ bindings/main.go
@@ -0,0 +1,21 @@
+package nuvioServer
+
+import (
+	server "server"
+)
+
+func StartTorrentServer(pathdb string, port int) int {
+	return server.Start(pathdb, port, false, false)
+}
+
+func WaitTorrentServer() {
+	server.WaitServer()
+}
+
+func StopTorrentServer() {
+	server.Stop()
+}
+
+func AddTrackers(trackers string) {
+	server.AddTrackers(trackers)
+}
